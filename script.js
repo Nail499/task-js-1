845b8ec84f84f04495ff714b8c47e5efceafd827
Nail499 <ibadovv079@gmail.com>
2023-11-02 12:39:17 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//// 1.Deyishenler Data1
+let = Nihadweight=78;
+let = Nihadheight=169;
+let = Nailweight=92;
+let = Nailheight=195;
+
+///// 2.BKI
+
+nibki = Nihadweight/(Nihadheight**2);
+nabki = Nailweight/(Nailheight**2);
+
+
+/// 3.muqayise
+
+if(nibki>nabki){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+/// 2.Data2
+
+let = Nihad1weight=95;
+let = Nihad1height=188;
+let = Nail1weight=85;
+let = Nail1height=76;
+
+ni1bki = Nihad1weight/(Nihad1height**2);
+na1bki = Nail1weight/(Nail1height**2);
+
+if(ni1bki>na1bki){
+    console.log(true);
+}else{
+    console.log(false);
+}
